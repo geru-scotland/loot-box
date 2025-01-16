5eb227af9e7c49171f266a3e31f45ac50af07939
geru-scotland <aingeru.scotland@gmail.com>
2025-01-16 15:17:15 +0100

    Constants: minor change.

--- constants/strings.js
+++ constants/strings.js
@@ -1,7 +1,7 @@
 const homepage = {
     TITLE: 'Loot Box',
     HEADER: "LOOT BOX",
-    DESCRIPTION: 'This is the homepage description',
+    DESCRIPTION: 'Welcome to a simple RNG loot box simulator.',
     LOGIN: 'Login',
     REGISTER: 'Register',
 };
