7e6ce7fca95c747a08cf613922aabc3b3af2cc28
geru-scotland <aingeru.scotland@gmail.com>
2025-01-22 13:21:52 +0100

    Fix item collection import on startup

--- db/connection.js
+++ db/connection.js
@@ -3,34 +3,37 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 const config = require("../config");
+const Item = require("../models/item.model")
 
 const importItems = async () => {
     try {
         const db = mongoose.connection.db;
 
-        const collections = await db.listCollections({ name: "items" }).toArray();
-        if (collections.length === 0) {
-            console.log("[MONGODB] La colección items no existe. Importando datos...");
+        const itemCount = await Item.countDocuments();
+
+        if (itemCount === 0) {
+            console.log("[MONGODB] Item colection does not exist, importing data...");
 
             const filePath = path.join(__dirname, "loot-box.items.json");
-            const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+            // Ignoro el id
+            const data = JSON.parse(fs.readFileSync(filePath, "utf8")).map(({ _id, ...rest }) => rest);
 
             await db.collection("items").insertMany(data);
-            console.log("[MONGODB] Datos importados correctamente.");
+            console.log("[MONGODB] Data imported successfully!");
         }
     } catch (error) {
-        console.error("Error al importar datos:", error);
+        console.error("Error importing data", error);
     }
 };
 
 const connection = async () => {
     try {
         await mongoose.connect(config.mongoURI);
-        console.log(`[MONGODB] Conectado a: ${config.mongoURI}`);
+        console.log(`[MONGODB] Connected: ${config.mongoURI}`);
 
         await importItems();
     } catch (error) {
-        console.error("Error en la conexión a MongoDB:", error);
+        console.error("Error connecting to DB:", error);
         process.exit(1);
     }
 };
